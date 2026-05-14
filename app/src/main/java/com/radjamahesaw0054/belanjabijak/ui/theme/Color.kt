01bb71f42d3935b8c512868cbda0d3da0956bd59
Radja Mahesa W <radjamahesaw@gmail.com>
2026-05-14 19:42:44 +0700

    Tema dan Warna aplikasi, dan Setup Strings.xml untuk multi-languange

---
+++ app/src/main/java/com/radjamahesaw0054/belanjabijak/ui/theme/Color.kt
@@ -0,0 +1,33 @@
+package com.radjamahesaw0054.belanjabijak.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val PrimaryLight = Color(0xFF006B4F)
+val OnPrimaryLight = Color(0xFFFFFFFF)
+val PrimaryContainerLight = Color(0xFF96F7CE)
+val OnPrimaryContainerLight = Color(0xFF002115)
+
+val SecondaryLight = Color(0xFF4A6357)
+val OnSecondaryLight = Color(0xFFFFFFFF)
+val SecondaryContainerLight = Color(0xFFCCE8DA)
+
+val TertiaryLight = Color(0xFF3B6470)
+val OnTertiaryLight = Color(0xFFFFFFFF)
+val TertiaryContainerLight = Color(0xFFBFE9F7)
+
+val BackgroundLight = Color(0xFFFBFDF9)
+val OnBackgroundLight = Color(0xFF191C1A)
+val SurfaceLight = Color(0xFFFBFDF9)
+val OnSurfaceLight = Color(0xFF191C1A)
+val SurfaceVariantLight = Color(0xFFDBE5DE)
+
+val PrimaryDark = Color(0xFF7ADAA3)
+val OnPrimaryDark = Color(0xFF003827)
+val PrimaryContainerDark = Color(0xFF00513A)
+val OnPrimaryContainerDark = Color(0xFF96F7CE)
+
+val BackgroundDark = Color(0xFF191C1A)
+val OnBackgroundDark = Color(0xFFE10E3)
+val SurfaceDark = Color(0xFF191C1A)
+val OnSurfaceDark = Color(0xFFE1E3E0)
---
+++ app/src/main/java/com/radjamahesaw0054/belanjabijak/ui/theme/Theme.kt
@@ -0,0 +1,50 @@
+package com.radjamahesaw0054.belanjabijak.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColorScheme = lightColorScheme(
+    primary = PrimaryLight,
+    onPrimary = OnPrimaryLight,
+    primaryContainer = PrimaryContainerLight,
+    onPrimaryContainer = OnPrimaryContainerLight,
+    secondary = SecondaryLight,
+    onSecondary = OnSecondaryLight,
+    secondaryContainer = SecondaryContainerLight,
+    tertiary = TertiaryLight,
+    onTertiary = OnTertiaryLight,
+    tertiaryContainer = TertiaryContainerLight,
+    background = BackgroundLight,
+    onBackground = OnBackgroundLight,
+    surface = SurfaceLight,
+    onSurface = OnSurfaceLight,
+    surfaceVariant = SurfaceVariantLight
+)
+
+private val DarkColorScheme = darkColorScheme(
+    primary = PrimaryDark,
+    onPrimary = OnPrimaryDark,
+    primaryContainer = PrimaryContainerDark,
+    onPrimaryContainer = OnPrimaryContainerDark,
+    background = BackgroundDark,
+    onBackground = OnBackgroundDark,
+    surface = SurfaceDark,
+    onSurface = OnSurfaceDark
+)
+
+@Composable
+fun BelanjaBijakTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
