1fb5abcb72f80b5dba7eb3f34a65e4d67d570c62
Radja Mahesa W <radjamahesaw@gmail.com>
2026-05-14 19:53:05 +0700

    Struktur Navigasi

---
+++ app/src/main/java/com/radjamahesaw0054/belanjabijak/navigation/NavGraph.kt
@@ -0,0 +1,56 @@
+package com.radjamahesaw0054.belanjabijak.navigation
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import com.radjamahesaw0054.belanjabijak.ui.screen.AddPurchaseScreen
+import com.radjamahesaw0054.belanjabijak.ui.screen.CompareProductScreen
+import com.radjamahesaw0054.belanjabijak.ui.screen.EditPurchaseScreen
+import com.radjamahesaw0054.belanjabijak.ui.screen.HomeScreen
+import com.radjamahesaw0054.belanjabijak.ui.screen.PurchaseHistoryScreen
+
+@Composable
+fun SetupNavGraph(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route,
+        modifier = modifier
+    ) {
+        composable(route = Screen.Home.route) {
+            HomeScreen(navController = navController)
+        }
+
+        composable(route = Screen.AddPurchase.route) {
+            AddPurchaseScreen(navController = navController)
+        }
+
+        composable(
+            route = Screen.EditPurchase.route,
+            arguments = listOf(
+                navArgument(KEY_ID_PURCHASE) {
+                    type = NavType.LongType
+                }
+            )
+        ) { backStackEntry ->
+            val id = backStackEntry.arguments?.getLong(KEY_ID_PURCHASE) ?: -1L
+            EditPurchaseScreen(navController = navController, idPurchase = id)
+        }
+
+        composable(route = Screen.PurchaseHistoryScreen.route) {
+            PurchaseHistoryScreen(navController = navController)
+        }
+
+        composable(route = Screen.CompareProduct.route) {
+            CompareProductScreen(navController = navController)
+        }
+    }
+}
+
---
+++ app/src/main/java/com/radjamahesaw0054/belanjabijak/navigation/Screen.kt
@@ -0,0 +1,13 @@
+package com.radjamahesaw0054.belanjabijak.navigation
+
+const val KEY_ID_PURCHASE = "idPurchase"
+
+sealed class Screen(val route: String) {
+    data object Home: Screen("mainScreen")
+    data object AddPurchase: Screen("AddPurchaseScreen")
+    data object EditPurchase: Screen("EditPurchaseScreen/{${KEY_ID_PURCHASE}}"){
+        fun withId(id: Long) = "EditPurchaseScreen/$id"
+    }
+    data object PurchaseHistoryScreen: Screen("PurchaseHistoryScreen")
+    data object CompareProduct: Screen("CompareProductScreen")
+}
